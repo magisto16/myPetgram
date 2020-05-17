6796debff3c785827905edf7dc16e8f59248d8c0
Angelica Kirana <aksd.70@hotmail.com>
2020-05-17 09:16:39 +0700

    Update ProfileItem.js

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -14,21 +14,11 @@ const ProfileItem = ({
       <img src={avatar} alt='' className='round-img' />
       <div>
         <h2>{name}</h2>
-        <p>
-          {status} {company && <span> at {company}</span>}
-        </p>
         <p className='my-1'>{location && <span>{location}</span>}</p>
         <Link to={`/profile/${_id}`} className='btn btn-primary'>
           View Profile
         </Link>
       </div>
-      <ul>
-        {skills.slice(0, 4).map((skill, index) => (
-          <li className='text-primary' key={index}>
-            <i className='fas fa-check' /> {skill}
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
